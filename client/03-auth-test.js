ecf9369cc9045efaefbf6ae2e54b8b38f720e79a
austinmscholl <asprof34@gmail.com>
2018-09-29 21:15:59 -0400

    reg update

--- client/03-auth-test.js
+++ client/03-auth-test.js
@@ -9,11 +9,36 @@ function fetchAllFromAuthRoute() {
             'Authorization': accessToken //4
         }
     })
-        .then(response => {
-            return response.json();
-        })
+    .then(response=>response.json())
+        
         .then(data => {
 
+            console.log(data)
+        })
+}
+
+/***************************************
+ * FETCH/POST to Auth/Create
+*************************************/
+function postToAuthRouteCreate() {
+    const fetch_url = `http://localhost:3000/authtest/create` 
+    const accessToken = localStorage.getItem('SessionToken')
+
+    let authTestDataInput = document.getElementById('authTestData').value; //1
+
+    let authInputData = { authtestdata: { item: authTestDataInput } }; //2
+
+    const response = fetch(fetch_url, {
+        method: 'POST', //3
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': accessToken
+        },
+        body: JSON.stringify(authInputData)  //4
+    })
+        .then(response=>response.json())
+        
+        .then(data => {
             console.log(data)
         })
 }
